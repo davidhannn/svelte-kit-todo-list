813dff916f2782e7d6ff6d16c34d477c5adab92c
David Han <davidhan2392@gmail.com>
2023-07-28 15:56:14 -0700

    using enhance for form submissions

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,67 @@
 <script>
+	import { fly, slide } from 'svelte/transition';
+	import { enhance } from '$app/forms';
+
 	export let data;
+	export let form;
+
+  let creating = false;
+	let deleting = [];
 </script>
 
 <div class="centered">
 	<h1>todos</h1>
 
-	<form method="POST" action="?/create">
+	{#if form?.error}
+		<p class="error">{form.error}</p>
+	{/if}
+
+	<form method="POST" action="?/create" use:enhance={() => {
+		creating = true;
+
+		return async ({ update }) => {
+			await update();
+			creating = false;
+		};
+	}}>
 		<label>
 			add a todo:
 			<input
+        disabled={creating}
 				name="description"
+				value={form?.description ?? ''}
 				autocomplete="off"
+				required
 			/>
 		</label>
 	</form>
 
-	<ul class="todos">
-		{#each data.todos as todo (todo.id)}
-			<li>
-				<form method="POST" action="?/delete">
-					<input type="hidden" name="id" value={todo.id} />
-					<span>{todo.description}</span>
-					<button aria-label="Mark as complete" />
-				</form>
-			</li>
-		{/each}
-	</ul>
+  <ul class="todos">
+    {#each data.todos.filter((todo) => !deleting.includes(todo.id)) as todo (todo.id)}
+      <li in:fly={{ y: 20 }} out:slide>
+        <form
+          method="POST"
+          action="?/delete"
+          use:enhance={() => {
+            deleting = [...deleting, todo.id];
+            return async ({ update }) => {
+              await update();
+              deleting = deleting.filter((id) => id !== todo.id);
+            };
+          }}
+        >
+          <input type="hidden" name="id" value={todo.id} />
+          <button aria-label="Mark as complete">✔</button>
+
+          {todo.description}
+        </form>
+      </li>
+    {/each}
+  </ul>
+
+  {#if creating}
+	<span class="saving">saving...</span>
+ {/if}
 </div>
 
 <style>
@@ -55,13 +91,5 @@
 		aspect-ratio: 1;
 		opacity: 0.5;
 		transition: opacity 0.2s;
-	}
-
-	button:hover {
-		opacity: 1;
-	}
-
-	.saving {
-		opacity: 0.5;
 	}
 </style>
